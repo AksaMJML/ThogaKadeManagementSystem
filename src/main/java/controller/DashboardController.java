fb9d44f19f1b5e60dcb6e2fe4c3e16d0cc9423f5
AksaMJML <akaks0186@gmail.com>
2026-01-07 11:23:32 +0530

    created buttons on dashboard and set stage to all

--- src/main/java/controller/DashboardController.java
+++ src/main/java/controller/DashboardController.java
@@ -2,10 +2,15 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashboardController {
-
+Stage stage=new Stage();
     @FXML
     private Button btnCustomer;
 
@@ -19,23 +24,43 @@ public class DashboardController {
     private Button btnOrderDetails;
 
     @FXML
-    void btnCustomerOnClick(ActionEvent event) {
-
+    void btnCustomerOnClick(ActionEvent event) throws IOException {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/customer_info.fxml"))));
+        stage.show();
+        stage.setTitle("CustomerView");
     }
 
     @FXML
     void btnItemOnClick(ActionEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/item_info.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+        stage.setTitle("ItemView");
     }
 
     @FXML
     void btnOrderDetailsOnClick(ActionEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/order_details_info.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+        stage.setTitle("orderDetailsView");
     }
 
     @FXML
     void btnOrderOnClick(ActionEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/order_info.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+        stage.setTitle("orderView");
     }
 
 }
