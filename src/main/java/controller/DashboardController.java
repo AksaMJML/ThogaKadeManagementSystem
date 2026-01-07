e583cf641f06a6c95c78ebd6b71f48418941cbfb
AksaMJML <akaks0186@gmail.com>
2026-01-07 10:56:02 +0530

    added buttons to dashbosrd

---
+++ src/main/java/controller/DashboardController.java
@@ -0,0 +1,42 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class DashboardController {
+
+    @FXML
+    private Button btnCustomer;
+
+    @FXML
+    private Button btnItem;
+
+    @FXML
+    private Button btnOrder;
+
+    @FXML
+    private Button btnOrderDetails;
+
+    @FXML
+    void btnCustomerOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderDetailsOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderOnClick(ActionEvent event) {
+
+    }
+
+}
+
